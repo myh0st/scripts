37320fc8ed594d47356052ca4d9c9134c5678faf
myh0st <1293348082@qq.com>
2023-06-23 15:20:28 +0800

    Update checkes.py

--- checkes.py
+++ checkes.py
@@ -10,7 +10,7 @@ def read_elastic(es_host, index):
     body = {'query': {'match_all':{}}}
     elastic = Elasticsearch(es_host)
     res = elastic.search(index=index, body=body, scroll='3m',  size=1, request_timeout=60)             
-    print(es_host, index, res)
+    print("[+]Datainfo: ", res)
         
 if __name__=="__main__":
     read_elastic(sys.argv[1], sys.argv[2])
