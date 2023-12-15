4ce7650a3c5ea1cab1fe698595a1f10543eb0a57
myh0st <1293348082@qq.com>
2023-12-15 10:28:49 +0800

    Update pbootcms-database-download.py

--- pbootcms-database-download.py
+++ pbootcms-database-download.py
@@ -45,7 +45,6 @@ def verify(site):
 
 if __name__=="__main__":
     target = sys.argv[1]
-    print("Microsoft Windows [版本 10.0.19044.3086]\n(c) Microsoft Corporation。保留所有权利。\n\nD:\VulnSubmit\script>python3 pbootcms-database-download.py ",target)
     info = verify(target)
     if info != "":
         print("[+]漏洞存在，读取到的管理员账号信息为：", info)
