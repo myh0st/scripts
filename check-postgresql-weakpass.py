6f17bb8cd7c0eec7f4d888622eae2b0579cb026b
myh0st <1293348082@qq.com>
2024-03-28 07:32:02 +0800

    Create check-postgresql-weakpass.py

---
+++ check-postgresql-weakpass.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#author: myh0st@xazlsec
+
+import sys
+import psycopg2
+
+
+def verify(siteinfo):
+    hostinfo, passinfo = siteinfo.split("/")[2].split("@")
+    host, port = hostinfo.split(":")
+    user, password = passinfo.split(":")
+    conn = psycopg2.connect(database="postgres", user=user, 
+                        password=password, host=host, 
+                        port=port)
+    with conn.cursor() as cur:
+        cur.execute("SELECT version();")
+        return cur.fetchall()
+
+    
+if __name__=="__main__":
+    target = sys.argv[1]
+    print("Microsoft Windows [版本 10.0.19044.3086]\n(c) Microsoft Corporation。保留所有权利。\n\nD:\VulnSubmit\script>python3 check-postgresql-weakpass.py ",target)
+    info = verify(target)
+    if info != "":
+        print("[+]漏洞存在，查询服务器信息为：", info)
+    else:
+        print("[-]漏洞不存在")
