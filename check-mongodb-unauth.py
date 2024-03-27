9d91b71e5ab6d135a987f168e8510b9d6db3a671
myh0st <1293348082@qq.com>
2024-03-27 16:27:02 +0800

    Create check-mongodb-unauth.py

---
+++ check-mongodb-unauth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#author: myh0st@xazlsec
+
+import sys
+from pymongo import MongoClient
+
+
+def verify(site):
+    ip = site.split("/")[2]
+    try:
+        client = MongoClient(ip,27017,connectTimeoutMS=1000,socketTimeoutMS=1000,waitQueueTimeoutMS=1000)
+        try:
+            serverInfo = client.server_info()
+        except:
+            pass
+        client.close()
+        return serverInfo
+    except:
+        pass
+
+    return ""
+    
+if __name__=="__main__":
+    target = sys.argv[1]
+    info = verify(target)
+    if info != "":
+        print("[+]漏洞存在，查询服务器信息为：", info)
+    else:
+        print("[-]漏洞不存在")
