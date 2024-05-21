1f2cd057f555900baaed72730f5a93da92440a2d
myh0st <1293348082@qq.com>
2024-05-21 08:32:22 +0800

    Create common-fileread.py

---
+++ common-fileread.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#author: myh0st@xazlsec
+
+
+import requests
+import urllib3
+import sys
+import time
+import json
+import re
+import urllib
+
+urllib3.disable_warnings()
+
+def verify(url):
+    burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8", "Content-Type": "application/json"}
+    r = requests.get(url, headers=burp0_headers, verify=False)
+    if r.status_code == 200:
+        return r.text[:1000]
+    return ""
+
+    
+if __name__=="__main__":
+    target = sys.argv[1]
+    info = verify(target)
+    if info != "":
+        print("[+]漏洞存在, 读取文件", payload, " 的结果为：", info)
+    else:
+        print("[-]漏洞不存在")
